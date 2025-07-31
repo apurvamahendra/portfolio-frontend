c0e6a6eccfc7dfd27add4d6839208d40f6e0e422
Nishchay Agarwal <nishchay.agar@gmail.com>
2025-07-31 19:53:20 +0530

    bug fixes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,8 +9,8 @@ const page = () => {
         <div className="text-center mb-16">
           <h1 className="text-5xl md:text-7xl font-serif mb-6">Get In Touch</h1>
           <p className="text-xl text-gray-300 max-w-2xl mx-auto">
-            I'd love to hear from you. Send me a message and I'll get back to
-            you as soon as possible.
+            I&apos;d love to hear from you. Send me a message and I&apos;ll get
+            back to you as soon as possible.
           </p>
         </div>
 
